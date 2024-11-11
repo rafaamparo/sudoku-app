e8accdb89ccf6b03364e41ccdd6c70bda38362bd
Rafael Amparo <rafa110304@gmail.com>
2024-11-11 11:43:27 -0300

    Refactor initial game setup UI to improve layout and enhance user experience

--- lib/sudoku_game.dart
+++ lib/sudoku_game.dart
@@ -53,76 +53,110 @@ class _PagSudokuState extends State<PagSudoku> {
   }
 
   Widget build(BuildContext context) {
-    return Center(
-        child: Column(
-      children: [
-        ConstrainedBox(
-          constraints: const BoxConstraints(maxWidth: 600),
-          child: Container(
-              padding: const EdgeInsets.all(5),
-              margin: const EdgeInsets.all(10),
-              alignment: Alignment.center,
-              child: GridView.builder(
-                itemCount: 9,
-                shrinkWrap: true,
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 3,
-                  childAspectRatio: 1,
-                  crossAxisSpacing: 4,
-                  mainAxisSpacing: 4,
-                ),
-                physics: const ScrollPhysics(),
-                itemBuilder: (BuildContext, matriz) {
-                  return Container(
-                      padding: const EdgeInsets.all(2),
-                      color: matriz % 2 == 0
-                          ? const Color.fromARGB(255, 98, 81, 120)
-                          : const Color.fromARGB(255, 109, 180, 222),
-                      alignment: Alignment.center,
-                      child: GridView.builder(
-                        itemCount: 9,
-                        shrinkWrap: true,
-                        gridDelegate:
-                            const SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: 3,
-                          childAspectRatio: 1,
-                          crossAxisSpacing: 5,
-                          mainAxisSpacing: 5,
-                        ),
-                        physics: ScrollPhysics(),
-                        itemBuilder: (BuildContext, indice) {
-                          return InkWell(
-                              onTap: () {
-                                print('Matriz: $matriz, Indice: $indice');
-                                setState(() {
-                                  if (celulasVazias
-                                      .contains(Tuple2(matriz, indice))) {
-                                    selecionado = [matriz, indice];
-                                  }
-                                });
-                              },
-                              child: Container(
-                                color: selecionado[0] == matriz &&
-                                        selecionado[1] == indice
-                                    ? const Color.fromARGB(255, 160, 225, 211)
-                                    : Colors.white,
-                                alignment: Alignment.center,
-                                child: Text(
-                                  matrizSudoku[matriz][indice] == -1
-                                      ? ''
-                                      : matrizSudoku[matriz][indice].toString(),
-                                  style: const TextStyle(
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                              ));
-                        },
-                      ));
-                },
-              )),
-        )
-      ],
-    ));
+    return SingleChildScrollView(
+        padding: const EdgeInsets.all(10),
+        child: Center(
+            child: Column(
+          children: [
+            ConstrainedBox(
+              constraints: const BoxConstraints(maxWidth: 600),
+              child: Container(
+                  padding: const EdgeInsets.all(5),
+                  margin: const EdgeInsets.all(10),
+                  alignment: Alignment.center,
+                  child: GridView.builder(
+                    itemCount: 9,
+                    shrinkWrap: true,
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 3,
+                      childAspectRatio: 1,
+                      crossAxisSpacing: 4,
+                      mainAxisSpacing: 4,
+                    ),
+                    physics: const ScrollPhysics(),
+                    itemBuilder: (BuildContext, matriz) {
+                      return Container(
+                          padding: const EdgeInsets.all(2),
+                          color: matriz % 2 == 0
+                              ? const Color.fromARGB(255, 98, 81, 120)
+                              : const Color.fromARGB(255, 109, 180, 222),
+                          alignment: Alignment.center,
+                          child: GridView.builder(
+                            itemCount: 9,
+                            shrinkWrap: true,
+                            gridDelegate:
+                                const SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 3,
+                              childAspectRatio: 1,
+                              crossAxisSpacing: 5,
+                              mainAxisSpacing: 5,
+                            ),
+                            physics: ScrollPhysics(),
+                            itemBuilder: (BuildContext, indice) {
+                              return InkWell(
+                                  onTap: () {
+                                    print('Matriz: $matriz, Indice: $indice');
+                                    setState(() {
+                                      if (celulasVazias
+                                          .contains(Tuple2(matriz, indice))) {
+                                        selecionado = [matriz, indice];
+                                      }
+                                    });
+                                  },
+                                  child: Container(
+                                    color: selecionado[0] == matriz &&
+                                            selecionado[1] == indice
+                                        ? const Color.fromARGB(
+                                            255, 160, 225, 211)
+                                        : celulasVazias.contains(
+                                                Tuple2(matriz, indice))
+                                            ? const Color.fromARGB(
+                                                255, 255, 255, 255)
+                                            : const Color.fromARGB(
+                                                255, 224, 205, 217),
+                                    alignment: Alignment.center,
+                                    child: Text(
+                                      matrizSudoku[matriz][indice] == -1
+                                          ? ''
+                                          : matrizSudoku[matriz][indice]
+                                              .toString(),
+                                      style: const TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ));
+                            },
+                          ));
+                    },
+                  )),
+            ),
+            Wrap(
+              direction: Axis.horizontal,
+              alignment: WrapAlignment.center,
+              spacing: 5,
+              runSpacing: 5,
+              children: [
+                OutlinedButton(onPressed: () {}, child: const Text('1')),
+                OutlinedButton(onPressed: () {}, child: const Text('2')),
+                OutlinedButton(onPressed: () {}, child: const Text('3')),
+                OutlinedButton(onPressed: () {}, child: const Text('4')),
+                OutlinedButton(onPressed: () {}, child: const Text('5')),
+                OutlinedButton(onPressed: () {}, child: const Text('6')),
+                OutlinedButton(onPressed: () {}, child: const Text('7')),
+                OutlinedButton(onPressed: () {}, child: const Text('8')),
+                OutlinedButton(onPressed: () {}, child: const Text('9')),
+                FilledButton(
+                    onPressed: () {},
+                    child: const Icon(Icons.delete_outline_sharp)),
+              ],
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            FilledButton(onPressed: () {}, child: const Text("Novo Jogo"))
+          ],
+        )));
   }
 }
--- lib/sudoku_iniciar.dart
+++ lib/sudoku_iniciar.dart
@@ -14,100 +14,102 @@ class _PagInicialState extends State<PagInicial> {
   @override
   Widget build(BuildContext context) {
     return Center(
-        child: ConstrainedBox(
-            constraints: const BoxConstraints(maxWidth: 800),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                const Text(
-                  'Prepare-se para jogar!',
-                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Padding(
-                    padding: const EdgeInsets.all(20),
-                    child: TextField(
-                      decoration: const InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'Seu Nome',
-                      ),
-                      controller: controladorNome,
-                    )),
-                const SizedBox(
-                  height: 20,
-                ),
-                Wrap(
-                  spacing: 10,
-                  runSpacing: 10,
-                  children: [
-                    RadioListTile(
-                      value: 'fácil',
-                      groupValue: dificuldade,
-                      onChanged: (String? value) {
-                        print("Radio: $value");
-                        dificuldade = value!;
-
-                        setState(() {});
-                      },
-                      title: const Text("Fácil"),
+        child: SingleChildScrollView(
+            child: ConstrainedBox(
+                constraints: const BoxConstraints(maxWidth: 800),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    const Text(
+                      'Prepare-se para jogar!',
+                      style:
+                          TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
                     ),
-                    RadioListTile(
-                      value: 'médio',
-                      groupValue: dificuldade,
-                      onChanged: (String? value) {
-                        print("Radio: $value");
-                        dificuldade = value!;
-                        setState(() {});
-                      },
-                      title: const Text("Médio"),
+                    const SizedBox(
+                      height: 20,
                     ),
-                    RadioListTile(
-                      value: 'difícil',
-                      groupValue: dificuldade,
-                      onChanged: (String? value) {
-                        print("Radio: $value");
-                        dificuldade = value!;
-
-                        setState(() {});
-                      },
-                      title: const Text("Difícil"),
+                    Padding(
+                        padding: const EdgeInsets.all(20),
+                        child: TextField(
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Seu Nome',
+                          ),
+                          controller: controladorNome,
+                        )),
+                    const SizedBox(
+                      height: 20,
                     ),
-                    RadioListTile(
-                      value: 'expert',
-                      groupValue: dificuldade,
-                      onChanged: (String? value) {
-                        print("Radio: $value");
-                        dificuldade = value!;
+                    Wrap(
+                      spacing: 10,
+                      runSpacing: 10,
+                      children: [
+                        RadioListTile(
+                          value: 'fácil',
+                          groupValue: dificuldade,
+                          onChanged: (String? value) {
+                            print("Radio: $value");
+                            dificuldade = value!;
 
-                        setState(() {});
+                            setState(() {});
+                          },
+                          title: const Text("Fácil"),
+                        ),
+                        RadioListTile(
+                          value: 'médio',
+                          groupValue: dificuldade,
+                          onChanged: (String? value) {
+                            print("Radio: $value");
+                            dificuldade = value!;
+                            setState(() {});
+                          },
+                          title: const Text("Médio"),
+                        ),
+                        RadioListTile(
+                          value: 'difícil',
+                          groupValue: dificuldade,
+                          onChanged: (String? value) {
+                            print("Radio: $value");
+                            dificuldade = value!;
+
+                            setState(() {});
+                          },
+                          title: const Text("Difícil"),
+                        ),
+                        RadioListTile(
+                          value: 'expert',
+                          groupValue: dificuldade,
+                          onChanged: (String? value) {
+                            print("Radio: $value");
+                            dificuldade = value!;
+
+                            setState(() {});
+                          },
+                          title: const Text("Expert"),
+                        )
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(
+                          context,
+                        ).push(MaterialPageRoute(
+                            builder: (context) => PagJogo(
+                                  dificuldadeSelecionada: dificuldade,
+                                  nome: controladorNome.text,
+                                )));
                       },
-                      title: const Text("Expert"),
+                      style: ButtonStyle(
+                          minimumSize: WidgetStateProperty.all(Size(100, 50)),
+                          backgroundColor: WidgetStateProperty.all(
+                              Color.fromARGB(255, 209, 154, 227))),
+                      child: const Text('Novo Jogo',
+                          style: TextStyle(color: Colors.white)),
                     )
                   ],
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.of(
-                      context,
-                    ).push(MaterialPageRoute(
-                        builder: (context) => PagJogo(
-                              dificuldadeSelecionada: dificuldade,
-                              nome: controladorNome.text,
-                            )));
-                  },
-                  style: ButtonStyle(
-                      minimumSize: WidgetStateProperty.all(Size(100, 50)),
-                      backgroundColor: WidgetStateProperty.all(
-                          Color.fromARGB(255, 209, 154, 227))),
-                  child: const Text('Novo Jogo',
-                      style: TextStyle(color: Colors.white)),
-                )
-              ],
-            )));
+                ))));
   }
 }
